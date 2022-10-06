b1682950141e3f36c3b109df3756a15aa04b62f7
Diego Alves <diegohsalves@gmail.com>
2022-10-06 20:55:55 -0300

    Customer service, controller, repository e routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
+import customersRouter from "@modules/Customers/routes/customer.routes";
 import productsRouter from "@modules/Products/routes/products.routes";
 import passwordRouter from "@modules/Users/routes/password.routes";
 import profileRouter from "@modules/Users/routes/profile.routes";
 import sessionsRouter from "@modules/Users/routes/sessions.routes";
 import usersRouter from "@modules/Users/routes/users.routes";
+
 import { Router } from "express";
 
 const routes = Router();
@@ -12,5 +14,6 @@ routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/password", passwordRouter);
 routes.use("/profile", profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
