60b3409ccc239f90197bcb179684bbb20aaab992
Diego Alves <diegohsalves@gmail.com>
2022-10-06 20:39:30 -0300

    Profile services, rotas e controller

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import productsRouter from "@modules/Products/routes/products.routes";
 import passwordRouter from "@modules/Users/routes/password.routes";
+import profileRouter from "@modules/Users/routes/profile.routes";
 import sessionsRouter from "@modules/Users/routes/sessions.routes";
 import usersRouter from "@modules/Users/routes/users.routes";
 import { Router } from "express";
@@ -10,5 +11,6 @@ routes.use("/products", productsRouter);
 routes.use("/users", usersRouter);
 routes.use("/sessions", sessionsRouter);
 routes.use("/password", passwordRouter);
+routes.use("/profile", profileRouter);
 
 export default routes;
